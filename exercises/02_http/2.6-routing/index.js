f2648ea5b4fc0b763b3947f61c110ed241c4d537
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-09-05 23:23:36 +0300

    updated not found message

--- exercises/02_http/2.6-routing/index.js
+++ exercises/02_http/2.6-routing/index.js
@@ -7,15 +7,15 @@ http.createServer((request, response) => {
 
   // Route requests based on the URL path
   if (urlPath === '/homer') {
-    readFileSendResponse('./homer.html', 'text/html', response);
+    readFileSendResponse('homer.html', 'text/html', response);
   } else if (urlPath === '/bradbury') {
-    readFileSendResponse('./bradbury.html', 'text/html', response);
+    readFileSendResponse('bradbury.html', 'text/html', response);
   } else if (urlPath === '/') {
-    readFileSendResponse('./index.html', 'text/html', response);
+    readFileSendResponse('index.html', 'text/html', response);
   } else {
     // For any other paths, respond with a 404 status
     response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.write('Not Found');
+    response.write('Requested content not found');
     response.end();
   }
 }).listen(3000);
@@ -24,7 +24,7 @@ const readFileSendResponse = (fileName, contentType, response) => {
   fs.readFile(fileName, (err, data) => {
     if (err) {
       response.writeHead(404, { 'Content-Type': 'text/plain' });
-      response.write('File not found');
+      response.write('Not found');
     } else {
       response.writeHead(200, { 'Content-Type': contentType });
       response.write(data);
