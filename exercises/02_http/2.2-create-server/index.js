0bbfd2e0cae6b7d6a06ad862f66cd0cdfd62e730
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-09-05 22:39:14 +0300

    update status code to 202

--- exercises/02_http/2.2-create-server/index.js
+++ exercises/02_http/2.2-create-server/index.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer(function(request, response) {
-  fs.readFile(path.resolve('index.html'), function(error, htmlPage) {
+  fs.readFile(path.resolve('./index.html'), function(error, htmlPage) {
     if (error) {
       response.writeHead(404);
       response.write('An error occured: ', error);
     } else {
-      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.writeHead(202, { 'Content-Type': 'text/html' });
       response.write(htmlPage);
     }
     response.end();
