4431ec8031b6a128fd9840194d87e0f6debee066
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-10-05 10:12:31 +0300

    6.4 Callbacks: timeout animation

--- exercises/06_async/ex4/callback.js
+++ exercises/06_async/ex4/callback.js
@@ -13,7 +13,14 @@
  * It draws only one arrow, the f expression must be 'drawArrow'
  * @param {*} i the index, the nth arrow to be drawn, note that the sequence has to return from destination back to the source
  */
-const drawArrows = (actors, timeout, drawArrow, i = 0) => {}
+const drawArrows = (actors, timeout, drawArrow, i = 0) => {
+  if(i === actors.length * 2 -2) {
+    return;
+  } else {
+      drawArrow(i, timeout, actors.length-1); //draw one arrow
+      setTimeout( () => drawArrows(actors, timeout, drawArrow, i+1), timeout);
+  }
+}
 
 
 
