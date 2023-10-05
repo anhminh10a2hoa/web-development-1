67f6d9bce97b88c1eed568db4df530c5a9b05de0
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-10-05 10:05:18 +0300

    updated test

--- exercises/06_async/ex3/test/f.test.js
+++ exercises/06_async/ex3/test/f.test.js
@@ -3,14 +3,13 @@ const expect = require("chai").expect;
 const f = require("../f_skeleton");
 
 describe("thenable", () => {
-  it("resolves with correct value", () => {
-    // TODO: Implement this test. See rejectable below for an example
-    // See: https://www.testim.io/blog/testing-promises-using-mocha/
-    // This test can be implemented with either Promises or with async / await, as shown on that page.
-    // Use expect() and to.equal()
-    // to make sure that the resolved value is 👍
-    throw "Not yet implemented!";
-  });
+  it("resolves with correct value", (done) => {
+    Promise.resolve(f.thenable)
+    .then((result) => {     
+      expect(result).to.equal("👍");
+      done();
+    });
+  }); 
 });
 
 describe("rejectable", () => {
